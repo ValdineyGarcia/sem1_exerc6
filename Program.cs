faf768822869fe22004b0880afa8ed3d110761ac
Valdiney Daniel Garcia <valdineysc@gmail.com>
2023-02-16 00:22:54 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int numInt;
+int resultado;
+Console.WriteLine("Informe um número inteiro, para o qual será calculado a taboada: ");
+numInt = int.Parse(Console.ReadLine());
+Console.WriteLine($"Esta é a taboada de {numInt}:");
+for (int i = 0; i<= 10; i++){
+    resultado = numInt * i;
+    Console.WriteLine($"{numInt} X {i} = {resultado}");
+}
